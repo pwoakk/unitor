c1741dac91c97c4ad947e93ddc137d6e76b5b522
Beka <becka.kg99@gmail.com>
2022-05-30 19:55:50 +0600

    add new app accounts and add postgre database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'unitor',
+        'USER': 'unitor_admin',
+        'PASSWORD': 'qwerty123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
